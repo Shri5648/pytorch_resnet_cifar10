a1eb5e3825138d84c5430cc86753f825b3bedeb2
Shreenidhi Srinivasan <156630615+Shri5648@users.noreply.github.com>
2025-09-14 15:43:33 -0400

    Update trainer.py (Import SCALE_optimizer.py)

--- trainer.py
+++ trainer.py
@@ -12,6 +12,7 @@ import torch.utils.data
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import resnet
+import SCALE_optimizer
 
 model_names = sorted(name for name in resnet.__dict__
     if name.islower() and not name.startswith("__")
