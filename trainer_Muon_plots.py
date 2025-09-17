03a671bcb2ab720bec999b31ab663e95b0b64381
Shreenidhi Srinivasan <156630615+Shri5648@users.noreply.github.com>
2025-09-17 12:51:48 -0400

    Update trainer_Muon_plots.py

--- trainer_Muon_plots.py
+++ trainer_Muon_plots.py
@@ -239,14 +239,14 @@ def train(train_loader, model, criterion, optimizers, epoch):
         batch_time.update(time.time() - end)
         end = time.time()
 
-        if i % args.print_freq == 0:
-            print('Epoch: [{0}][{1}/{2}]\t'
-                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})'.format(
-                      epoch, i, len(train_loader), batch_time=batch_time,
-                      data_time=data_time, loss=losses, top1=top1))
+        #if i % args.print_freq == 0:
+        #    print('Epoch: [{0}][{1}/{2}]\t'
+        #          'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+        #          'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+        #          'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+        #          'Prec@1 {top1.val:.3f} ({top1.avg:.3f})'.format(
+        #              epoch, i, len(train_loader), batch_time=batch_time,
+        #              data_time=data_time, loss=losses, top1=top1))
 
     return top1.avg
 
