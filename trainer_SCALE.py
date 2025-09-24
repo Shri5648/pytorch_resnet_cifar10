1e64154ff76ecabf8879f4bb0cde61400909099d
Shreenidhi Srinivasan <156630615+Shri5648@users.noreply.github.com>
2025-09-24 10:38:52 -0400

    Update trainer_SCALE.py

--- trainer_SCALE.py
+++ trainer_SCALE.py
@@ -357,3 +357,8 @@ def accuracy(output, target, topk=(1,)):
 
 if __name__ == '__main__':
     main()
+
+    text_file_name='SCALE_results.txt'
+    with open(text_file_name, "w") as f:
+        f.write("File written by trainer_SCALE.py\n")
+        f.write(f"train_prec1_history={train_prec1_history},\n" f"val_prec1_history={val_prec1_history}")
