02bb34a136ad11c65bf79f46146e6025eae693e1
Shreenidhi Srinivasan <156630615+Shri5648@users.noreply.github.com>
2025-09-23 22:42:03 -0400

    Update trainer_Muon_plots.py

--- trainer_Muon_plots.py
+++ trainer_Muon_plots.py
@@ -14,7 +14,6 @@ import torchvision.datasets as datasets
 import resnet
 import matplotlib.pyplot as plt
 from Muon_optimizer import Muon
-from plots.py import 
 
 model_names = sorted(name for name in resnet.__dict__
     if name.islower() and not name.startswith("__")
@@ -344,3 +343,7 @@ def accuracy(output, target, topk=(1,)):
 
 if __name__ == '__main__':
     main()
+    with open(text_file_name, "w") as f:
+        f.write("File written by trainer_Muon.py\n")
+        f.write(f"train_prec1_history={train_prec1_history},val_prec1_history={val_prec1_history}")
+    
