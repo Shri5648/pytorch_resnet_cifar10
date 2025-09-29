3241d799e9029c8dbebc7ff6026da296149458c1
Shreenidhi Srinivasan <156630615+Shri5648@users.noreply.github.com>
2025-09-29 08:26:40 -0400

    Update trainer_Muon_plots.py

--- trainer_Muon_plots.py
+++ trainer_Muon_plots.py
@@ -345,6 +345,6 @@ if __name__ == '__main__':
     main()
     matlab_file_name='Muon_results.m'
     with open(matlab_file_name, "w") as f:
-        f.write("%File written by trainer_Muon.py\n")
+        f.write("%File written by trainer_Muon_plots.py\n")
         f.write(f"train_prec1_history={train_prec1_history};\n" f"val_prec1_history={val_prec1_history};")
     
