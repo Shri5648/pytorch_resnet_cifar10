bb7ff2af1182b5405ad6fbb3cf7702fdcb743bb2
Shreenidhi Srinivasan <156630615+Shri5648@users.noreply.github.com>
2025-10-07 22:23:31 -0400

    Update Muon_pnorm.py

--- Muon_pnorm.py
+++ Muon_pnorm.py
@@ -12,44 +12,62 @@ class Muon_pnorm(torch.optim.Optimizer):
     def __init__(self, params, lr=0.02, weight_decay=0.01, momentum=0.95, p=2.0):
         defaults = dict(lr=lr, weight_decay=weight_decay, momentum=momentum,p=p)
         super().__init__(params, defaults)
-    
+
+
     @torch.no_grad()
     def step(self):
         for group in self.param_groups:
-            p_exp = group['p']
-            for p_tensor in group['params']:
-                if p_tensor.grad is None:
+            p=group['p']
+            for p in group['params']:
+                if p.grad is None:
                     continue
-                G = p_tensor.grad
-                state = self.state[p_tensor]
+                grad = p.grad
+                state = self.state[p]
                 
-                # Initialize momentum buffer and step counter
                 if len(state) == 0:
-                    state['momentum_buffer'] = torch.zeros_like(G)
-                    state['step'] = 0
+                    state['momentum_buffer'] = torch.zeros_like(grad)
+                    #state['step']=...
                 
                 momentum_buffer = state['momentum_buffer']
+                p.mul_(1 - group['lr'] * group['weight_decay'])
+                momentum_buffer.lerp_(grad, 1 - group['momentum'])
+                grad = grad.lerp_(momentum_buffer, group['momentum'])
                 
-                # Weight decay (in-place)
-                p_tensor.mul_(1 - group['lr'] * group['weight_decay'])
-                
-                # Momentum update
-                momentum_buffer.lerp_(G, 1 - group['momentum'])
-                blended_grad = G.lerp(momentum_buffer, group['momentum'])
-                
-                # Compute SVD of blended gradient
-                U, S, Vh = SVD_exact(blended_grad)
-                
+                # Compute SVD of gradient
+                U, S, Vh = SVD_exact(grad)
+
                 # Compute modified singular values: S^{1/p - 1}
-                print("p=",p_exp);
-                exp = 1.0 / p_exp - 1.0
+                exp = 1.0 / p - 1.0
                 S_mod = torch.pow(S, exp)
-                
-                # Reconstruct search direction D = -U · diag(S_mod) · Vh
-                D = -U.matmul(torch.diag(S_mod)).matmul(Vh)
-                
+
+                # Reconstruct search direction d = -U · diag(S_mod) · Vh
+                d = -U.matmul(torch.diag(S_mod)).matmul(Vh)
+
                 # Update parameters along custom SVD direction
-                p_tensor.add_(D, alpha=group['lr'])
+                p.add_(d, alpha=group['lr'])
+
+                 # Increment step counter
+                # state['step'] += 1
+
                 
-                # Increment step counter
-                state['step'] += 1
+              
+
+   
+                
+              
+                
+                
+                
+                             
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+               
