7f3118583d9be69c93f35e3832f3240a214ee012
Shreenidhi Srinivasan <156630615+Shri5648@users.noreply.github.com>
2025-10-14 22:07:24 -0400

    Update trainer_Muon_AdamW_pnorm.py

--- trainer_Muon_AdamW_pnorm.py
+++ trainer_Muon_AdamW_pnorm.py
@@ -356,7 +356,7 @@ def accuracy(output, target, topk=(1,)):
 
 if __name__ == '__main__':
     main()
-    matlab_file_name='Muon_AdamW_2norm_results.m'
+    matlab_file_name='Muon_AdamW_1.5norm_results.m'
     with open(matlab_file_name, "w") as f:
         f.write("%File written by trainer_Muon_AdamW_pnorm.py\n")
         f.write(f"Total training time for {args.epochs} epochs: {total_training_time:.2f};\n")
