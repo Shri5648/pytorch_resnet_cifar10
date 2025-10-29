d000edcfbc26a9c0af883df07ff9d05bed439460
Shreenidhi Srinivasan <156630615+Shri5648@users.noreply.github.com>
2025-10-28 23:25:37 -0400

    Fix import statement for Muon_pnorm

--- trainer_Muon_AdamW_pnorm.py
+++ trainer_Muon_AdamW_pnorm.py
@@ -14,7 +14,7 @@ import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import resnet
 import matplotlib.pyplot as plt
-from Muon_pnorm import Muon_pnorm
+from Muon_pnorm(p>1) import Muon_pnorm
 
 model_names = sorted(name for name in resnet.__dict__
     if name.islower() and not name.startswith("__")
