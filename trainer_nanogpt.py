b175cf8eab4f5c89039806b1ae339c7e98958bfd
Shreenidhi Srinivasan <156630615+Shri5648@users.noreply.github.com>
2025-11-07 10:28:38 -0500

    Update trainer_nanogpt.py

--- trainer_nanogpt.py
+++ trainer_nanogpt.py
@@ -11,7 +11,7 @@ import torch.optim
 import torch.utils.data
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
-from .read_datasets import read_datasets
+from .read_Shakespeare_dataset import read_datasets
 #import resnet
 import nanogpt
 
